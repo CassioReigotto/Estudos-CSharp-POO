3fea4e59ea23f21ca83c6e368b6aedb6eb57d6df
CassioReigotto <cassio.reigotto@outlook.com>
2025-01-06 16:37:12 -0400

    Add project files.

---
+++ 01Atributos/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Atributos
+{
+    class Pessoa
+    {
+        public int idade;
+        public string nome;
+
+        public void mensagem()
+        {
+            Console.WriteLine("Olá {0}, tudo bom, sua idade é {1}", nome, idade);
+        }
+
+    }
+}
---
+++ 01Atributos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01Atributos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.idade = 1;
+            pessoa.nome = "Cassio";
+            pessoa.mensagem();
+        }
+    }
+}
